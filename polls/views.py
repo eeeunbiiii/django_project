c1268fa2c0e3f1cf5a63b2ccb353371722f35229
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-05-10 22:01:59 +0900

    form patch

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5] 
@@ -14,9 +14,28 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-def results(reaquest, question_id):
-    response = "You're looking at the results of questions %s."
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("Yor're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        #reques.POST : 파이썬 사전과 비슷한 오브젝트 => choice오브젝트의 ID를 문자열로 리턴
+        #request.POST의 값들은 항상 문자열
+        #POST데이터에 choice가 없으면 KeyError발생
+    except (KeyError, Choice.DoesNotExist):
+        # 에러 메시지와 함께 폼을 다시 보여줌
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message' : "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1 #초이스 카운트 증가
+        selected_choice.save()
+        # HttpREsponseRedirect 리턴 (POST 데이터를 성공적으로 핸들링 했을때 항상 redirect)
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    # reverse()함수는 우리가 보고싶은 뷰의 이름과 뷰를 가리키는 url 패턴의 변수를 받는다
+    # 위 같은 경우 , /polls/3/results/를 리턴하게 됨
