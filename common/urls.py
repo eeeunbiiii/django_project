1b309d522bf6d20861f782e34663e8517ad798fb
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-05-14 01:29:44 +0900

    login 해결

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 app_name = "common"
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
 ]
