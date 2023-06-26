b72b727c8103ea47cf9ad4507a87d0a261c111df
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-06-26 22:33:09 +0900

    add post_write

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('write/', views.post_write, name='post_write'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 def post_list(request):
-    return render(request, 'post/post_list.html')
+    login_session = request.session.get('login_session', '')
+    context = {'login_session' : login_session}
 
+    return render(request, 'post/post_list.html', context)
+
+
+def post_write(request):
+    login_session = request.session.get('login_session', '')
+    context = {'login_session' : login_session}
+
+    return render(request, 'post/post_write.html', context)
 # Create your views here.
