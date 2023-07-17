842d2530f1b8f472b7bd34860b172227f813c4b7
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-07-17 12:17:49 +0900

    velog model migrate

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class Velog(models.Model):
     title = models.CharField(max_length=70)
+    author = models.TextField(default='')
+    date = models.TextField(default='')
     
     def __str__(self):
         return self.title
